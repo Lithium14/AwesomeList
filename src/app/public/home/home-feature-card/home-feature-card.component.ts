8fa94975db5f3e4500cf45ffc8f64777c2580446
Lithium14 <fadi.rata@live.fr>
2019-12-21 10:29:54 +0100

    generate componenet home

---
+++ src/app/public/home/home-feature-card/home-feature-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-feature-card',
+  templateUrl: './home-feature-card.component.html',
+  styles: []
+})
+export class HomeFeatureCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
